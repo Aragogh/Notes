a3d878840c640db37b7fc079c8960ca48ca55b54
Aragogh <avc.techalchemy@gmail.com>
2018-09-27 10:06:55 -0700

    First commit for notes

---
+++ Summer2018REU/auto/Assignment.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "Assignment"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite")
+   (TeX-add-symbols
+    '("sous" 1)
+    '("sur" 1)
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-bibliographies
+    "REU")
+   (LaTeX-add-amsthm-newtheorems
+    "lemma"
+    "exercise"))
+ :latex)
+
