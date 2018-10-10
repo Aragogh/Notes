16c7b7231072ac39143aab03e759214bec053f44
Aragogh <avc.techalchemy@gmail.com>
2018-10-10 12:30:19 -0700

    Notes

---
+++ 225A/Homework1/auto/Homework1.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "Homework1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite")
+   (TeX-add-symbols
+    '("sous" 1)
+    '("sur" 1)
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-amsthm-newtheorems
+    "lemma"
+    "exercise"))
+ :latex)
+
