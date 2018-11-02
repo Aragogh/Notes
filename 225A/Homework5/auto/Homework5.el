6793dead99abcc249e089139869fc30ee1d86973
Aragogh <avc.techalchemy@gmail.com>
2018-11-02 05:46:03 -0700

    Notes

---
+++ 225A/Homework5/auto/Homework5.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "Homework5"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite")
+   (TeX-add-symbols
+    '("sous" 1)
+    '("sur" 1)
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-amsthm-newtheorems
+    "lemma"
+    "exercise"))
+ :latex)
+
