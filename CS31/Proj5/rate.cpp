584e9ebe838a4bb9f47fcd7d11a8b6072c18c3f9
Aragogh <avc.techalchemy@gmail.com>
2018-11-19 00:33:39 -0800

    Notes

--- CS31/Proj5/rate.cpp
+++ CS31/Proj5/rate.cpp
@@ -33,10 +33,37 @@ bool fuzzyStrCmp(char a1[], char a2[]){
     return !strcmp(comp1, comp2);
 }
 
+bool isWord(char word1[]){
+    int j=0;
+    while(word1[j] != '\0'){
+            if(!isalpha(word1[j]))
+                return false;
+            j++;
+    }
+    return true;
+}
+
+bool checkifdupe(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1], int separation[], int n, int& f)
+{
+    if(n == 0)
+        return false;
+    for(int i=n-1; i > -1; i--){
+        if((fuzzyStrCmp(word1[n], word1[i]) && fuzzyStrCmp(word2[n], word2[i])) || (fuzzyStrCmp(word1[n], word2[i]) && fuzzyStrCmp(word2[n], word1[i]))){
+            if(separation[n] > separation[i])
+                f = i;
+            else
+                f = n;
+            return true;
+        }
+    }
+    return false;
+}
+
 int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1], int separation[], int nPatterns)
 {
     int n = nPatterns; 
     int i = 0;
+    int dupevar;
     if(nPatterns < 0)
         n = 0;
     while(i < n){
@@ -44,9 +71,45 @@ int makeProper(char word1[][MAX_WORD_LENGTH+1], char word2[][MAX_WORD_LENGTH+1],
             killrow(word1, word2, separation, n, i);
             continue;
             }
+        if(!isWord(word1[i]) || !isWord(word2[i])){
+            killrow(word1, word2, separation, n, i);
+            continue;
+        }
+        checkifdupe(word1, word2, separation, i, dupevar);
+        if(checkifdupe(word1, word2, separation, i, dupevar)){
+            killrow(word1, word2, separation, n, dupevar);
+            continue;
+        }
         i++;
-    }       
-    return n-1;
+    }
+    
+    return n;
+}
+
+int find(const char document[], const char a1[MAX_WORD_LENGTH+1])
+{
+    int n = strlen(document);
+    int n1 = strlen(a1);
+    int l = 0;
+    for(int i=0; i < n; i++){
+        if(document[i] == a2[0]){
+            while(a1[l] && l+i < n && document[l+i] == a2[l])
+                l++;
+            }
+    if(l == n1){
+      k = i;
+      break;
+    }
+  }
+  if(l != n2)
+    return -1;
+  return k;
+    }
+}
+
+int rate(const char document[], const char word1[][MAX_WORD_LENGTH+1], const char word2[][MAX_WORD_LENGTH+1], const int separation[], int nPatterns)
+{
+    
 }
 
 int main(){
@@ -55,16 +118,16 @@ int main(){
     strcpy(a2, "lavida");
     assert(fuzzyStrCmp(a1, a2) == 0);
     
-    int TEST1_NRULES = 4;
+    const int TEST1_NRULES = 5;
     char test1w1[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
-        "mad", "deranged", "nefarious", "have"
+        "mad", "deranged", "nefarious", "PloT", "have"
     };
     char test1w2[TEST1_NRULES][MAX_WORD_LENGTH+1] = {
-        "scientist", "robot", "plot", "mad"
+        "scientist", "robot", "plot", "nEfArious", "mad"
     };
-    int test1dist[TEST1_NRULES] = {-1,-3,0,1};
-    makeProper(test1w1, test1w2, test1dist, 4);
-    for(int i = 0; i <= makeProper(test1w1, test1w2, test1dist, TEST1_NRULES); i++){
+    int test1dist[TEST1_NRULES] = {1,3,3,4,1};
+    int k = makeProper(test1w1, test1w2, test1dist, TEST1_NRULES);
+    for(int i = 0; i < k; i++){
         cout << test1w1[i] << " " << test1w2[i] << " " << test1dist[i] << endl;
     }
     cout << "All tests succeeded!" << endl;
