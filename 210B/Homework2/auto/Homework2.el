c1d919499391b006f4f28f6a61086c7ee77f075b
Aragogh <avc.techalchemy@gmail.com>
2019-04-24 09:26:47 -0700

    Notes

---
+++ 210B/Homework2/auto/Homework2.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "Homework2"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite"
+    "tikz-cd"
+    "mathrsfs")
+   (TeX-add-symbols
+    '("sous" 1)
+    '("sur" 1)
+    "Hom"
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-amsthm-newtheorems
+    "lemma"
+    "exercise"))
+ :latex)
+
---
+++ 225B/Homework7/auto/Homework7.el
@@ -0,0 +1,38 @@
+(TeX-add-style-hook
+ "Homework7"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite"
+    "tikz-cd"
+    "mathrsfs")
+   (TeX-add-symbols
+    '("Claim" 1)
+    '("sous" 1)
+    '("sur" 1)
+    "Hom"
+    "st"
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-environments
+    "lemma"
+    "exercise"))
+ :latex)
+
