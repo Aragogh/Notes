0845ed989dfd13fce277a22e1847524999dbbda5
Aragogh <avc.techalchemy@gmail.com>
2019-06-08 20:26:19 -0700

    Notes

---
+++ 210C/Final/auto/Final.el
@@ -0,0 +1,41 @@
+(TeX-add-style-hook
+ "Final"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite"
+    "tikz-cd"
+    "mathrsfs")
+   (TeX-add-symbols
+    '("fk" 1)
+    '("bb" 1)
+    '("sous" 1)
+    '("sur" 1)
+    "Hom"
+    "Tor"
+    "Ext"
+    "iso"
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-environments
+    "lemma"
+    "exercise"))
+ :latex)
+
