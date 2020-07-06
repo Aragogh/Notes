2d2a6b5e13989ef8438b5306ed24feeac1d7f730
Aragogh <avc.techalchemy@gmail.com>
2020-07-06 00:49:43 -0700

    Notes

---
+++ 246A/Homework6/auto/Homework6.el
@@ -0,0 +1,44 @@
+(TeX-add-style-hook
+ "Homework6"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm" "plain")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "fancyhdr"
+    "extramarks"
+    "amsmath"
+    "amsthm"
+    "amssymb"
+    "amsfonts"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "nameref"
+    "cite"
+    "tikz-cd"
+    "mathrsfs")
+   (TeX-add-symbols
+    '("fk" 1)
+    '("bb" 1)
+    '("sous" 1)
+    '("sur" 1)
+    '("circled" 1)
+    "Hom"
+    "Tor"
+    "Ext"
+    "iso"
+    "del"
+    "conj"
+    "hmwkTitle"
+    "hmwkDueDate"
+    "hmwkClass"
+    "hmwkClassInstructor"
+    "hmwkAuthorName")
+   (LaTeX-add-environments
+    "lemma"
+    "exercise"))
+ :latex)
+
