0fae7310653eba630963df2df961a3f7a75ba05e
Aragogh <avc.techalchemy@gmail.com>
2020-09-12 14:27:09 -0500

    Notes

---
+++ MyNotes/riehlCategoricalHomotopyTheory2014/internals.pl
@@ -0,0 +1,46 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/sec:org60b6a1b/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org61aedd0/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org6647d13/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org75f2f11/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org90c14cc/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org93d23d1/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orga936a8d/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orgac79b58/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orgb7f3352/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orge8ff365/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ MyNotes/riehlCategoricalHomotopyTheory2014/labels.pl
@@ -0,0 +1,53 @@
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# Associate labels original text with physical files.
+
+
+$key = q/sec:org60b6a1b/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org61aedd0/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org6647d13/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org75f2f11/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org90c14cc/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:org93d23d1/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orga936a8d/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orgac79b58/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orgb7f3352/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:orge8ff365/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2019 (Released January 1, 2019)
+# labels from external_latex_labels array.
+
+
+1;
+
